b3cc944b503a78997158c053adba1313a45bba7b
xamfoo <xamfoo@icloud.com>
2025-11-08 17:22:07 +0800

    Update PATH and set SHELL

--- early-init.el
+++ early-init.el
@@ -1,7 +1,17 @@
 (when (string-equal system-type "android")
   ;; Add Termux binaries to PATH environment
   (let* ((termux-prefix "/data/data/com.termux")
-	 (termux-path (concat termux-prefix "/files/usr/bin")))
-    (setenv "PATH" (concat (getenv "PATH") ":" termux-path))
-    (setq exec-path (append exec-path (list termux-path)))
-    (setenv "HOME" (concat termux-prefix "/files/home"))))
+         (termux-path (concat termux-prefix "/files/usr/bin"))
+         (termux-home (concat termux-prefix "/files/home"))
+         (termux-shell (concat termux-path "/bash"))
+         (termux-local-bin (concat termux-home "/.local/bin"))
+         (path-dirs (list
+		     termux-local-bin
+                     termux-path)))
+    (setenv
+     "PATH"
+     (mapconcat 'identity path-dirs ":"))
+    (setq exec-path (append path-dirs exec-path))
+    (setenv "SHELL" termux-shell)
+    (setq shell-file-name termux-shell)
+    (setenv "HOME" termux-home)))
